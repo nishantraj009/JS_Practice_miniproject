6f6cb12eb43a3b22042a7c514ba7a6fe58683036
nishant <nishantraj2052004@gmail.com>
2025-12-21 08:38:28 +0530

    updated

---
+++ 04_mousehouver/app.js
@@ -0,0 +1,18 @@
+let p = document.querySelector("p");
+ 
+p.addEventListener("click",function(){
+    console.log("paragraph was clicked");
+});
+
+let divbox = document.getElementById("box");
+
+divbox.addEventListener("mouseenter",function(){
+    alert("the mouse touched to the dic box container");
+});
+
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click",function(){
+    console.dir(this);
+});
